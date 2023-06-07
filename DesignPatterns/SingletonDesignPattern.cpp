3b136251a0f495821743407ed11b9825987d2efd
Ashwin <ashwinstalin2002@gmail.com>
2023-06-07 15:57:45 +0530

    Update SingletonDesignPattern.cpp

--- DesignPatterns/SingletonDesignPattern.cpp
+++ DesignPatterns/SingletonDesignPattern.cpp
@@ -2,13 +2,11 @@
 
 class DataBase{
     private:
+        static DataBase *db;
         DataBase(){}
         int arr[4];
     public:
-        static DataBase& getInstance(){
-            static DataBase db;
-            return db;
-        }
+        static DataBase* getInstance();
         void makeArray(int x, int y, int z){
             arr[0] = x;
             arr[1] = y;
@@ -25,13 +23,62 @@ class DataBase{
         }
 };
 
+DataBase *DataBase::db = nullptr;
+DataBase *DataBase::getInstance(){
+    if(db == nullptr){
+        db = new DataBase();
+        return db;
+    }else{
+        return db;
+    }
+}
+
 int main(){
-    DataBase& db = DataBase::getInstance();
-    db.makeArray(4,5,6);
-    db.show();
-    DataBase& db2 = DataBase::getInstance();
-    db2.lastElement(7);
-    db2.show();
+    DataBase* db = DataBase::getInstance();
+    db->makeArray(4,5,6);
+    db->show();
+    DataBase* db2 = DataBase::getInstance();
+    db2->lastElement(7);
+    db2->show();
 
     return 0;
 }
+
+
+// #include<iostream>
+
+// class DataBase{
+//     private:
+//         DataBase(){}
+//         int arr[4];
+//     public:
+//         static DataBase& getInstance(){
+//             static DataBase db;
+//             return db;
+//         }
+//         void makeArray(int x, int y, int z){
+//             arr[0] = x;
+//             arr[1] = y;
+//             arr[2] = z;
+//         }
+//         void lastElement(int x){
+//             arr[3] = x;
+//         }
+//         void show(){
+//             for(int x : arr){
+//                 std::cout << x << " ";
+//             }
+//             std::cout << std::endl;
+//         }
+// };
+
+// int main(){
+//     DataBase& db = DataBase::getInstance();
+//     db.makeArray(4,5,6);
+//     db.show();
+//     DataBase& db2 = DataBase::getInstance();
+//     db2.lastElement(7);
+//     db2.show();
+
+//     return 0;
+// }
