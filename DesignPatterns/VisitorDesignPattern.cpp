652958a6e74203341b266a32471755bbcadc6324
Ashwin <ashwinstalin2002@gmail.com>
2023-06-09 15:50:52 +0530

    Update VisitorDesignPattern.cpp

--- DesignPatterns/VisitorDesignPattern.cpp
+++ DesignPatterns/VisitorDesignPattern.cpp
@@ -1,76 +1,46 @@
-#include <iostream>
+#include<iostream>
 
-class Mouse;
-class Keyboard;
-class Monitor;
+class Animal;
+class Shape;
 
-class ComputerPartVisitor {
+class Visitor{
     public:
-        virtual void visit(Mouse* mouse) = 0;
-        virtual void visit(Keyboard* keyboard) = 0;
-        virtual void visit(Monitor* monitor) = 0;
+        virtual void visit(Animal *animal) = 0;
+        virtual void visit(Shape *shape) = 0;
 };
 
-class ComputerPart {
+class Element{
     public:
-        virtual void accept(ComputerPartVisitor* computerPartVisitor) = 0;
+        virtual void accept(Visitor *visitor) = 0;
 };
 
-class Mouse : public ComputerPart {
+class Animal : public Element{
     public:
-        void accept(ComputerPartVisitor* computerPartVisitor){
-            computerPartVisitor->visit(this);
-        }
-};
-
-class Keyboard : public ComputerPart {
-    public:
-        void accept(ComputerPartVisitor* computerPartVisitor){
-            computerPartVisitor->visit(this);
+        void accept(Visitor *visitor){
+            visitor->visit(this);
         }
 };
 
-class Monitor : public ComputerPart {
+class Shape : public Element{
     public:
-        void accept(ComputerPartVisitor* computerPartVisitor){
-            computerPartVisitor->visit(this);
+        void accept(Visitor *visitor){
+            visitor->visit(this);
         }
 };
 
-class Computer : public ComputerPart {
-    private:
-        std::vector<ComputerPart*> parts;
+class ElementVisitor: public Visitor{
     public:
-        Computer() {
-            parts.push_back(new Mouse());
-            parts.push_back(new Keyboard());
-            parts.push_back(new Monitor());
-        }
-        void accept(ComputerPartVisitor* computerPartVisitor){
-            for (ComputerPart *part : parts) {
-                part->accept(computerPartVisitor);
-            }
-        }
+        void visit(Animal *animal){
+            std::cout << "Animal" << std::endl;
+        } 
+        void visit(Shape *shape){
+            std::cout << "Shape" << std::endl;
+        } 
 };
 
-class ComputerPartDisplayVisitor : public ComputerPartVisitor {
-    public:
-        void visit(Mouse* mouse) {
-            std::cout << "Displaying Mouse" << std::endl;
-        }
-
-        void visit(Keyboard* keyboard) {
-            std::cout << "Displaying Keyboard" << std::endl;
-        }
-
-        void visit(Monitor* monitor) {
-            std::cout << "Displaying Monitor" << std::endl;
-        }
-};
-
-int main() {
-    ComputerPart* computer = new Computer();
-    ComputerPartVisitor* computerPartDisplay = new ComputerPartDisplayVisitor();
-    computer->accept(computerPartDisplay);
+int main(){
+    Element *element = new Animal();
+    Visitor *eVisitor = new ElementVisitor();
+    element->accept(eVisitor);
     return 0;
 }
