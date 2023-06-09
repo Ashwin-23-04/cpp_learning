c36d97b81f027d54873a2bbaeb858bc97c5f43da
Ashwin <ashwinstalin2002@gmail.com>
2023-06-09 16:05:49 +0530

    Update VisitorDesignPattern.cpp

--- DesignPatterns/VisitorDesignPattern.cpp
+++ DesignPatterns/VisitorDesignPattern.cpp
@@ -1,46 +1,69 @@
 #include<iostream>
 
-class Animal;
-class Shape;
+class Circle;
+class Rectangle;
 
 class Visitor{
     public:
-        virtual void visit(Animal *animal) = 0;
-        virtual void visit(Shape *shape) = 0;
+        virtual void visit(Rectangle *rectangle) = 0;
+        virtual void visit(Circle *circle) = 0;
 };
 
-class Element{
+class Shape{
     public:
         virtual void accept(Visitor *visitor) = 0;
 };
 
-class Animal : public Element{
+class Rectangle : public Shape{
+    private:
+        double width;
+        double height;
+        
     public:
+        Rectangle(double width, double height){
+            this->width = width;
+            this->height = height;
+        }
         void accept(Visitor *visitor){
             visitor->visit(this);
         }
-};
-
-class Shape : public Element{
-    public:
-        void accept(Visitor *visitor){
-            visitor->visit(this);
+        double getWidth(){
+            return width;
+        }
+        double getHeight(){
+            return height;
         }
 };
 
-class ElementVisitor: public Visitor{
+class Circle : public Shape{
+    private:
+        double radius;
     public:
-        void visit(Animal *animal){
-            std::cout << "Animal" << std::endl;
+        Circle(double radius){
+            this->radius = radius;
+        }
+        void accept(Visitor *visitor){
+            visitor->visit(this);
+        }
+        double getRadius(){
+            return radius;
+        }
+};
+
+class ShapeVisitor: public Visitor{
+    public:
+        void visit(Rectangle *rectangle){
+            std::cout << "Area of Rectangle is " << rectangle->getWidth() * rectangle->getHeight()  << std::endl;
         } 
-        void visit(Shape *shape){
-            std::cout << "Shape" << std::endl;
+        void visit(Circle *circle){
+            std::cout << "Area of Circle is " << 3.1415 * circle->getRadius() * circle->getRadius()  << std::endl;
         } 
 };
 
 int main(){
-    Element *element = new Animal();
-    Visitor *eVisitor = new ElementVisitor();
-    element->accept(eVisitor);
+    // Shape *shape = new Rectangle(2,3);
+    Shape *shape = new Circle(2);
+    Visitor *eVisitor = new ShapeVisitor();
+    shape->accept(eVisitor);
     return 0;
 }
